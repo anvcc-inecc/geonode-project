847078fab0b120832af72d61196ffb108db81cdf
Niji <mathereall@gmail.com>
2024-07-29 19:43:52 -0600

    update frontend

--- docker/nginx/atlas/assets/use-quasar.768ecafc.js
+++
@@ -1,1 +0,0 @@
-import{i as a,a0 as r}from"./index.ffd877c6.js";function u(){return a(r)}export{u};
---
+++ docker/nginx/atlas/assets/use-quasar.be3cb8c1.js
@@ -0,0 +1,1 @@
+import{i as a,a0 as r}from"./index.f8fb5b5b.js";function u(){return a(r)}export{u};
